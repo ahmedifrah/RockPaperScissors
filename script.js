37c1259dd3bab9a61914875a57bf7f5ee6ed2ce3
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-10-15 14:50:46 +0500

    add function to play 5 rounds and reports a winner or loser at the end

--- script.js
+++ script.js
@@ -8,28 +8,58 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
 
-    console.log(computerSelection);
+
 
     if (playerSelection === computerSelection){
+        tie += 1;
         return `You tied!`;
     }else {
         if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper") ){
+            win += 1;
             return `You Won! ${playerSelection} beats ${computerSelection}`;
     
         } else {
+            
+            loss +=1;
             return `You Lost! ${computerSelection} beats ${playerSelection}`;
         }
     }
+}
 
-    
+function game(){
 
+    for (let i = 1; i < 6; i++){
+        let playerSelection = prompt("Rock, Paper or Scissors?")
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+  
+    }
+
+    if (win > loss && tie){
+        console.log('You are the winner!');
+    } else if (win === loss){
+        console.log( 'You tied!');
+    }
+    else {
+        console.log('You Lost!')
+    }
+
+    console.log(`You won: ${win} round(s) \n
+                 You Lost: ${loss} round(s) \n
+                 You Tied: ${tie} round(s)`);
 
 
 }
+let win = 0;
+let loss = 0;
+let tie = 0;
+
+console.log(game())
 
 
 
-const playerSelection = "Scissors";
-const computerSelection = getComputerChoice();
+// let playerSelection = prompt("Rock, Paper or Scissors?");
+// let computerSelection = getComputerChoice();
+// console.log(game());
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
