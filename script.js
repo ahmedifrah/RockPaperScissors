abf756e248ccb89eb09b22142a214ff4ded5fa4a
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-10-15 13:02:41 +0500

    add logic if playerSelection is 'Rock'

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function getComputerChoice(){
     return choiceValue;
     
 }
+
+function playRound(playerSelection, computerSelection){
+
+    console.log(computerSelection);
+
+    if (playerSelection === computerSelection){
+        return `You tied!`;
+    }else {
+        if (playerSelection === "Rock" && computerSelection === "Scissors"){
+            return `You Won! ${playerSelection} beats ${computerSelection}`;
+    
+        } else {
+            return `You Lost! ${computerSelection} beats ${playerSelection}`;
+        }
+    }
+
+    
+
+
+
+}
+
+
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
