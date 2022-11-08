9311895c55daf754e47ea88ba10e1c3ec26a1e66
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2022-11-08 11:27:52 +0500

    add UI to Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function playRound(playerSelection, computerSelection){
 
     if (playerSelection === computerSelection){
         tie += 1;
-        return `You tied!`;
+        return `You tied! You both took ${playerSelection}`;
     }else {
         if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper") ){
             win += 1;
@@ -26,40 +26,56 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-
-    for (let i = 1; i < 6; i++){
-        let playerSelection = prompt("Rock, Paper or Scissors?")
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-  
-    }
-
-    if (win > loss && tie){
-        console.log('You are the winner!');
-    } else if (win === loss){
-        console.log( 'You tied!');
-    }
-    else {
-        console.log('You Lost!')
-    }
-
-    console.log(`You won: ${win} round(s) \n
-                 You Lost: ${loss} round(s) \n
-                 You Tied: ${tie} round(s)`);
-
-
-}
 let win = 0;
 let loss = 0;
 let tie = 0;
 
-console.log(game())
+function handleClick(){
+
+    const value = this.textContent;
+    const container = document.querySelector('.container')
+
+    for (let i = 1; i < 6; i++){
+ 
+            let playerSelection = value;
+            let computerSelection = getComputerChoice();
+            const text = (playRound(playerSelection, computerSelection));
+            const div = document.createElement('div');
+            div.textContent = text;
+            container.appendChild(div);
+      
+        
+
+    }
+
+    if (win > loss && tie){
+        const result = document.createElement('div');
+        result.textContent = ('You are the winner !');
+        container.appendChild(result);
+    } else if (win === loss){
+        const result = document.createElement('div');
+        result.textContent = ('You tied !');
+        container.appendChild(result);
+    }
+    else {
+        const result = document.createElement('div');
+        result.textContent = ('You Lost !');
+        container.appendChild(result);
+    }
+
+    const score = document.createElement('div');
+
+    score.textContent = (`You won: ${win} round(s) \n
+                 You Lost: ${loss} round(s) \n
+                 You Tied: ${tie} round(s)`);
+
+    container.appendChild(score);
+    
+}
 
 
 
-// let playerSelection = prompt("Rock, Paper or Scissors?");
-// let computerSelection = getComputerChoice();
-// console.log(game());
-
-// console.log(playRound(playerSelection, computerSelection));
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', handleClick, false)
+});
